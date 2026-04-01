ac7832ab6cd0437325969e6a9d935725e9a6730a
sushmiithaa <rsushmiithaa@gmail.com>
2026-04-01 14:06:58 +0800

    Update mark/unmark functionality to use mark event and mark occurrence

--- src/main/java/seedu/duke/command/MarkCommand.java
+++ src/main/java/seedu/duke/command/MarkCommand.java
@@ -1,8 +1,17 @@
 package seedu.duke.command;
 
 import seedu.duke.appcontainer.AppContainer;
+import seedu.duke.exception.UniTaskerException;
+import seedu.duke.task.Event;
+import seedu.duke.tasklist.EventReference;
+
 import seedu.duke.ui.ErrorUi;
+import seedu.duke.ui.GeneralUi;
 import seedu.duke.ui.TaskUi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class MarkCommand implements Command {
     public static final int MARK_MIN_LENGTH = 4;
@@ -36,6 +45,9 @@ public class MarkCommand implements Command {
                 break;
             case "event":
                 handleEvent(container);
+                break;
+            case "occurrence":
+                handleOccurrence(container);
                 break;
             default:
                 ErrorUi.printUnknownCommand("mark/unmark", "todo, deadline or event");
@@ -87,11 +99,58 @@ public class MarkCommand implements Command {
     private void handleEvent(AppContainer container) {
         try {
             Result result = getResult(container);
-            container.categories().setEventStatus(result.categoryIndex, result.taskIndex, isMark);
-            TaskUi.printStatusChanged(container.categories()
-                    .getEvent(result.categoryIndex, result.taskIndex).toString(), isMark);
+            String[] validViews = {"EVENT", "EVENT_EXPANDED", "NORMAL_EVENT_ONLY"};
+            validateView(container,validViews,"To mark/unmark a specific event please " +
+                    "use 'list event' or 'list event /all' first. " +
+                    "To mark/unmark a occurrence use 'list occurrence' then 'mark/unmark occurrence'");
+            EventReference ref = getEventReference(container, result);
+            Event event = container.categories().getEvent(ref.categoryIndex, ref.eventIndex);
+            if (event.getIsRecurring() &&
+                    (!container.categories().getCurrentView().equals("EVENT_EXPANDED"))) {
+                GeneralUi.printBordered("This is a recurring group. To mark/unmark the specific occurrence, please " +
+                        "use 'list event /all' or 'list occurrence " +
+                        (result.categoryIndex + 1) + " " + (result.taskIndex + 1) + "' first");
+            } else {
+                setStatusAndPrintMessage(container, ref, event);
+            }
         } catch (Exception e) {
             ErrorUi.printError(e.getMessage());
         }
+    }
+
+    private void handleOccurrence(AppContainer container) {
+        try {
+            Result result = getResult(container);
+            String[] validViews = {"OCCURRENCE_VIEW"};
+            validateView(
+                    container, validViews,
+                    "To mark/unmark a occurrence please " +
+                            "use 'list occurrence' first to see individual events of recurring group");
+            EventReference ref = getEventReference(container, result);
+            Event event = container.categories().getEvent(ref.categoryIndex, ref.eventIndex);
+            setStatusAndPrintMessage(container, ref, event);
+        } catch (Exception e) {
+            ErrorUi.printError(e.getMessage());
+        }
+    }
+
+    private void setStatusAndPrintMessage(AppContainer container, EventReference ref, Event event) {
+        container.categories().setEventStatus(ref.categoryIndex, ref.eventIndex, isMark);
+        TaskUi.printStatusChanged(event.toString(), isMark);
+    }
+
+    private static void validateView(AppContainer container, String[] views,
+            String errorMessage) throws UniTaskerException {
+        String currentView = container.categories().getCurrentView();
+        boolean isInvalid = !(Arrays.asList(views).contains(currentView));
+        if (isInvalid) {
+            throw new UniTaskerException(errorMessage);
+        }
+    }
+
+    private static EventReference getEventReference(AppContainer container, Result result) {
+        Map<Integer, List<EventReference>> map = container.categories().getActiveDisplayMap();
+        List<EventReference> categoryMap = map.get(result.categoryIndex);
+        return categoryMap.get(result.taskIndex);
     }
 }
